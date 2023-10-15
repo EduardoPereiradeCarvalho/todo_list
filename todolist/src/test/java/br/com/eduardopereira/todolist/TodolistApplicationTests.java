864221d693d62050ce5c5facf2da662354667bce
Eduardo Pereira <eduardopereira@gmail.com>
2023-10-14 22:20:43 -0300

    Dockerfile deploy

---
+++ todolist/src/test/java/br/com/eduardopereira/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eduardopereira.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
